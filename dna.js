1d54ba0c4566e542334ff2ac53935603bfa7c359
Clay Heaton <clayheaton@clays-mbp.rtp.rti.org>
2016-07-13 11:41:28 -0400

    fixed casing

--- dna.js
+++ dna.js
@@ -1,118 +1,118 @@
-function DNA(){
-  this.bg           = null;
-  this.figures      = [];
+function DNA() {
+  this.bg = null;
+  this.figures = [];
   this.pl_structure = null;
-  this.punchline    = "Text not assigned."
-  
+  this.punchline = "Text not assigned."
+
   // After the initial population, use this to breed "parents"
-  this.initWithGenes = function(dna1,dna2){
+  this.initWithGenes = function(dna1, dna2) {
 
     // Pick the background
-    if(random(0,1) <= mutationChance){
+    if (random(0, 1) <= mutationChance) {
       // New random background
       this.setRandomBackground();
     } else {
       // 50/50 chance of getting from one of the "parents"
-      if(random(0,1) <= 0.5){
+      if (random(0, 1) <= 0.5) {
         this.bg = dna1.bg;
       } else {
         this.bg = dna2.bg;
       }
     }
-    
+
     // Pick the figures
-    if(random(0,1) <= mutationChance){
+    if (random(0, 1) <= mutationChance) {
       // New random figures
       this.figures = [];
       this.setRandomFigures();
     } else {
       // 50/50 chance of getting figures from one of the "parents"
-      if(random(0,1) <= 0.5){
+      if (random(0, 1) <= 0.5) {
         this.figures = dna1.figures;
       } else {
         this.figures = dna2.figures;
       }
     }
-    
+
     // Pick the punchline structure
-    if(random(0,1) <= mutationChance){
+    if (random(0, 1) <= mutationChance) {
       // New random punchline structure
       this.setRandomPunchlineStructure();
     } else {
       // 50/50 chance of getting the punchline structure from one of the "parents"
-      if(random(0,1) <= 0.5){
+      if (random(0, 1) <= 0.5) {
         this.pl_structure = dna1.pl_structure;
       } else {
         this.pl_structure = dna2.pl_structure;
       }
-    } 
-    
+    }
+
     // Pick the punchline 
-    if(random(0,1) <= mutationChance){
+    if (random(0, 1) <= mutationChance) {
       // New random punchline 
       this.createRandomPunchline();
     } else {
       // 50/50 chance of getting the punchline from one of the "parents"
-      if(random(0,1) <= 0.5){
+      if (random(0, 1) <= 0.5) {
         this.punchline = dna1.punchline;
       } else {
         this.punchline = dna2.punchline;
       }
-    } 
-    
+    }
+
   }
-  
+
   // We need to randomly assign a background in the initial population
-  this.setRandomBackground = function(){
-    this.bg = backgrounds[Math.floor(Math.random()*backgrounds.length)];
+  this.setRandomBackground = function() {
+    this.bg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
   }
-  
-  
+
+
   // We need to randomly populate figures in the initial population
-  this.setRandomFigures = function(){
-    var numPeople = floor(random(minPeople,maxPeople + 1));
-    var a = shuffle(people,false);
-    
+  this.setRandomFigures = function() {
+    var numPeople = floor(random(minPeople, maxPeople + 1));
+    var a = shuffle(people, false);
+
     // Identify slots for the people and shuffle
-    var slots          = [];
-    for (var k = 0; k < numPeople; k++){
-      slots.push(k+1);
+    var slots = [];
+    for (var k = 0; k < numPeople; k++) {
+      slots.push(k + 1);
     }
     var shuffled_slots = shuffle(slots);
-    
-    for (var i = 0; i < numPeople; i++){
+
+    for (var i = 0; i < numPeople; i++) {
       var p = new Person(a[i]);
-      
-      var randomScale = random(minScale,maxScale);
-      p.figureScale   = randomScale;
-      
+
+      var randomScale = random(minScale, maxScale);
+      p.figureScale = randomScale;
+
       // x positioning
-      p.positionSlot  = shuffled_slots[i];
+      p.positionSlot = shuffled_slots[i];
       p.determineX(numPeople);
-      
-      var randomY     = random(minY,maxY);
-      p.pos.y         = randomY;
-      
+
+      var randomY = random(minY, maxY);
+      p.pos.y = randomY;
+
       this.figures.push(p)
     }
   }
-  
+
   // This randomly sets a grammatical structure for a punchline.
-  this.setRandomPunchlineStructure = function(){
-    this.pl_structure = punchline_structures[Math.floor(Math.random()*punchline_structures.length)];
+  this.setRandomPunchlineStructure = function() {
+    this.pl_structure = punchline_structures[Math.floor(Math.random() * punchline_structures.length)];
   }
-  
+
   // This creates a random punchline based on the structure available.
-  this.createRandomPunchline = function(){
+  this.createRandomPunchline = function() {
     this.punchline = "";
-    for (var w = 0; w < this.pl_structure.length; w++){
+    for (var w = 0; w < this.pl_structure.length; w++) {
       var word = this.randomWordForType(this.pl_structure[w]);
       this.punchline += word;
-      if (w < this.pl_structure.length - 1){
-        var next_structure = this.pl_structure[w+1];
-        if (next_structure == "." || 
-            next_structure == ":" || 
-            next_structure == ","){
+      if (w < this.pl_structure.length - 1) {
+        var next_structure = this.pl_structure[w + 1];
+        if (next_structure == "." ||
+          next_structure == ":" ||
+          next_structure == ",") {
           // No space following  next_structure == "''" || next_structure == '``'
           continue;
         } else {
@@ -120,7 +120,7 @@ function DNA(){
         }
       }
     }
-    
+
     // Clean up the generated punchline.
     // This isn't working  - need to use regex.
     // this.punchline = this.punchline.replace(" i."," I.");
@@ -128,40 +128,40 @@ function DNA(){
     // 
     this.punchline = this.applySentenceCase(this.punchline);
     this.punchline = this.punchline.replace(/i(?=[\s.,!"'\b])/g, "I");
-    this.punchline = this.punchline.replace(" 's","'s");
-    this.punchline = this.punchline.replace("s ' ","s' ");
-    this.punchline = this.punchline.replace(/(I is )/g,"I am ");
-    this.punchline = this.punchline.replace(/(Him is )/g,"He is ");
-    this.punchline = this.punchline.replace(/(hatfield)/g,"Hatfield");
-    this.punchline = this.punchline.replace(/(mccoy)/g,"McCoy");
-    this.punchline = this.punchline.replace(/(peter)/g,"Peter");
-    this.punchline = this.punchline.replace(/(jeb)/g,"Jeb");
-    this.punchline = this.punchline.replace(/(freshdirect)/g,"Freshdirect");
-    this.punchline = this.punchline.replace(/(cctv)/g,"CCTV");
-    this.punchline = this.punchline.replace(/(dumbledore)/g,"Dumbledore");
+    this.punchline = this.punchline.replace(" 's", "'s");
+    this.punchline = this.punchline.replace("s ' ", "s' ");
+    this.punchline = this.punchline.replace(/(I is )/g, "I am ");
+    this.punchline = this.punchline.replace(/(Him is )/g, "He is ");
+    // this.punchline = this.punchline.replace(/(hatfield)/g,"Hatfield");
+    // this.punchline = this.punchline.replace(/(mccoy)/g,"McCoy");
+    // this.punchline = this.punchline.replace(/(peter)/g,"Peter");
+    // this.punchline = this.punchline.replace(/(jeb)/g,"Jeb");
+    // this.punchline = this.punchline.replace(/(freshdirect)/g,"Freshdirect");
+    // this.punchline = this.punchline.replace(/(dumbledore)/g,"Dumbledore");
+    this.punchline = this.punchline.replace(/(cctv)/g, "CCTV");
     // print(this.punchline);
   }
-  
+
   // This method is used to create the initial population of comics. 
-  this.initAsRandom = function(){
+  this.initAsRandom = function() {
     this.setRandomBackground();
     this.setRandomFigures();
     this.setRandomPunchlineStructure();
     this.createRandomPunchline();
   }
-  
+
   // Returns a word matching a grammatical type
-  this.randomWordForType = function(word_type){
+  this.randomWordForType = function(word_type) {
     var wl = punchline_vocabulary[word_type];
-    var w  = wl[Math.floor(Math.random()*wl.length)];
+    var w = wl[Math.floor(Math.random() * wl.length)];
     return w;
   }
-  
+
   // http://stackoverflow.com/a/20442069/373402
   this.applySentenceCase = function(str) {
-    return str.replace(/.+?[\.\?\!](\s|$)/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    return str.replace(/.+?[\.\?\!](\s|$)/g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1);
     });
-}
-  
+  }
+
 }
